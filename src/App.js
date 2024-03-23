ca3e6815b5b1e132de49a12a4f9f2b90add02d74
Nikola Perica <np46120@oss.unist.hr>
2024-03-23 12:36:54 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { getMonth } from './uitl';
+import React, { useState, useContext, useEffect } from 'react';
+import CalendarHeader from './components/CalendarHeader';
+import SideBar from './components/SideBar';
+import Month from './components/Month';
+import GlobalContext from './context/GlobalContext';
+
+function App() {
+  console.table(getMonth())
+  const [currentMonth, setCurrentMonth]=useState(getMonth())
+  const {monthIndex}=useContext(GlobalContext)
+
+  useEffect(()=>{
+    setCurrentMonth(getMonth(monthIndex))
+  }, [monthIndex])
+
+  return (
+    <React.Fragment>
+      <div className='h-screen flex flex-col'>
+        <CalendarHeader />
+        <div className='flex flex-1'>
+          <SideBar />
+          <Month month={currentMonth}/>
+
+        </div>
+
+      </div>
+
+    </React.Fragment>
+  );
+}
+
+export default App;
