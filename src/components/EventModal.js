31f37816bfdcd9c6dc4185de8d52ab36c0e82dd0
Nikola Perica <np46120@oss.unist.hr>
2024-03-28 13:42:21 +0100

    save event to local storage
    created saving event to local storage

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -2,9 +2,33 @@ import React, { useState } from 'react'
 import { useContext } from 'react'
 import GlobalContext from '../context/GlobalContext'
 
+const labelsClasses = [
+    "indigo",
+    "gray",
+    "green",
+    "blue",
+    "red",
+    "purple",
+  ];
+
 export default function EventModal() {
-    const {setShowEventModal, daySelected}=useContext(GlobalContext)
-    const {title, setTitle}=useState("")
+    const {setShowEventModal, daySelected, dispatchCalEvent}=useContext(GlobalContext)
+    const [title, setTitle]=useState("")
+    const [description, setDescription]=useState("")
+    const [selectedLabel, setSelectedLabel]=useState(labelsClasses[0])
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const calendarEvent={
+            title,
+            description,
+            label: selectedLabel,
+            day: daySelected.valueOf(),
+            id: Date.now()
+        }
+        dispatchCalEvent({type: "push", payload: calendarEvent})
+        setShowEventModal(false)
+    }
 
   return (
     <div className='h-screen w-full fixed left-0 top-0 flex justify-center items-center'>
@@ -27,8 +51,32 @@ export default function EventModal() {
                     schedule
                     </span>
                     <p>{daySelected.format("dddd, MMMM DD")}</p>
+                    <span className='material-icons-outlined text-gray-400'>
+                    segment
+                    </span>
+                    <input type="text" name="description" placeholder='Add description' value={description} onChange={(e)=>setDescription(e.target.value)} required className='pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500' />
+                    <span className='material-icons-outlined text-gray-400'>
+                    bookmark_border
+                    </span>
+                    <div className="flex gap-x-2">
+                    {labelsClasses.map((lblClass, i) => (
+                        <span
+                        key={i}
+                        onClick={()=>setSelectedLabel(lblClass)}
+                        className={`bg-${lblClass}-500 w-6 h-6 rounded-full flex items-center justify-center cursor-pointer`}
+                        >
+                            {selectedLabel===lblClass && <span className="material-icons-outlined text-white text-sm">check</span>}
+                        
+                        </span>
+                    ))}
+                    </div>
                 </div>
             </div>
+            <footer className='flex justify-end border-t p-3 mt-5'>
+                <button type='submit' onClick={handleSubmit} className='bg-blue-500 hover:bg-blue-600 px-6 py-2 rounded text-white'>
+                        Save
+                </button>
+            </footer>
         </form>
     </div>
   )
